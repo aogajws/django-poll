6bc656cfd785b99ceb557a0587028f68936f0882
strangerxxxx <dendeneden@gmail.com>
2021-09-14 23:57:03 +0900

    hw

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
